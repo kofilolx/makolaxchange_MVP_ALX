e6568dde8f90398b77e02e5da956ba64bedaca63
KOTEY Jonathan Nikoi <32402172+kofilolx@users.noreply.github.com>
2024-07-04 22:45:41 +0000

    views.py

--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
+
+@views.route('/terms')
+def terms():
+    return render_template("terms.html")
